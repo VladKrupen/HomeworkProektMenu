7dfb93942d4d93999237979030e4f90f5f69551d
Vlad Krupen <air7360@gmail.com>
2023-10-28 23:02:44 +0300

    Made a graphical interface

--- HomeworkMenu/ViewController.swift
+++ HomeworkMenu/ViewController.swift
@@ -12,10 +12,10 @@ class Menu {
     
     let snacks: MenuItem
     let mainMenu: MenuItem
-    let beverages: MenuItem
+    let beverages: [MenuItem]
     let dessert: MenuItem
     
-    init(snacks: MenuItem, mainMenu: MenuItem, beverages: MenuItem, dessert: MenuItem) {
+    init(snacks: MenuItem, mainMenu: MenuItem, beverages: [MenuItem], dessert: MenuItem) {
         self.snacks = snacks
         self.mainMenu = mainMenu
         self.beverages = beverages
@@ -77,14 +77,15 @@ class Napoleon: MenuItem {
 
 let sendwitch = Sandwitch(name: "Бутерброд с курицей", price: 7)
 let steik = Steik(name: "Стэйк средней прожарки", price: 40)
-let coffee = Coffee(name: "Латте", price: 8)
-let napoleon = Napoleon(name: "Чизкейк", price: 10)
+let coffeeLatte = Coffee(name: "Латте", price: 8)
+let napoleon = Napoleon(name: "Наполеон", price: 10)
+let coffeeAmericano = Coffee(name: "Американо", price: 10)
 
 func printMenuItem(menuItem: MenuItem) {
     print("\(menuItem.name) \(menuItem.price) рублей")
 }
 
-
+let menu = Menu(snacks: sendwitch, mainMenu: steik, beverages: [coffeeLatte, coffeeAmericano], dessert: napoleon)
 
 
 
@@ -92,9 +93,107 @@ func printMenuItem(menuItem: MenuItem) {
 
 
 class ViewController: UIViewController {
+    
+    let textLabel = UILabel()
+    
+    let buttonSendwitch = {
+        let buttonSendwitch = UIButton(type: .system)
+        buttonSendwitch.setTitle(sendwitch.name, for: .normal)
+        return buttonSendwitch
+    }()
+    let buttonSteik = {
+        let buttonSteik = UIButton(type: .system)
+        buttonSteik.setTitle(steik.name, for: .normal)
+        return buttonSteik
+    }()
+    let buttonCoffeeLatte = {
+        let buttonCoffeeLatte = UIButton(type: .system)
+        buttonCoffeeLatte.setTitle(coffeeLatte.name, for: .normal)
+        return buttonCoffeeLatte
+    }()
+    let buttonNapoleon = {
+        let buttonNapoleon = UIButton(type: .system)
+        buttonNapoleon.setTitle(napoleon.name, for: .normal)
+        return buttonNapoleon
+    }()
+    let buttonCoffeeAmericano = {
+        let buttonCoffeeAmericano = UIButton(type: .system)
+        buttonCoffeeAmericano.setTitle(coffeeAmericano.name, for: .normal)
+        return buttonCoffeeAmericano
+    }()
+    let buttonStart = {
+        let buttonStart = UIButton(type: .system)
+        buttonStart.setTitle("Start", for: .normal)
+        return buttonStart
+    }()
+    let buttonEnd = {
+        let buttonEnd = UIButton(type: .system)
+        buttonEnd.setTitle("End", for: .normal)
+        return buttonEnd
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemTeal
+        
+        view.addSubview(buttonSendwitch)
+        view.addSubview(buttonSteik)
+        view.addSubview(buttonCoffeeLatte)
+        view.addSubview(buttonNapoleon)
+        view.addSubview(buttonCoffeeAmericano)
+        view.addSubview(buttonStart)
+        view.addSubview(buttonEnd)
+        
+        buttonCoffeeLatte.tintColor = .purple
+        buttonCoffeeLatte.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        buttonCoffeeLatte.translatesAutoresizingMaskIntoConstraints = false
+        buttonCoffeeLatte.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        buttonCoffeeLatte.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        buttonSteik.tintColor = .purple
+        buttonSteik.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        buttonSteik.translatesAutoresizingMaskIntoConstraints = false
+        buttonSteik.centerXAnchor.constraint(equalTo: buttonCoffeeLatte.centerXAnchor).isActive = true
+        buttonSteik.centerYAnchor.constraint(equalTo: buttonCoffeeLatte.centerYAnchor, constant: -50).isActive = true
+        
+        buttonSendwitch.tintColor = .purple
+        buttonSendwitch.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        buttonSendwitch.translatesAutoresizingMaskIntoConstraints = false
+        buttonSendwitch.centerXAnchor.constraint(equalTo: buttonSteik.centerXAnchor).isActive = true
+        buttonSendwitch.centerYAnchor.constraint(equalTo: buttonSteik.centerYAnchor, constant: -50).isActive = true
+        
+        buttonNapoleon.tintColor = .purple
+        buttonNapoleon.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        buttonNapoleon.translatesAutoresizingMaskIntoConstraints = false
+        buttonNapoleon.centerXAnchor.constraint(equalTo: buttonCoffeeLatte.centerXAnchor).isActive = true
+        buttonNapoleon.centerYAnchor.constraint(equalTo: buttonCoffeeLatte.centerYAnchor, constant: 50).isActive = true
+        
+        buttonCoffeeAmericano.tintColor = .purple
+        buttonCoffeeAmericano.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        buttonCoffeeAmericano.translatesAutoresizingMaskIntoConstraints = false
+        buttonCoffeeAmericano.centerXAnchor.constraint(equalTo: buttonNapoleon.centerXAnchor).isActive = true
+        buttonCoffeeAmericano.centerYAnchor.constraint(equalTo: buttonNapoleon.centerYAnchor, constant: 50).isActive = true
+        
+        buttonStart.backgroundColor = .green
+        buttonStart.layer.cornerRadius = 15
+        buttonStart.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        buttonStart.translatesAutoresizingMaskIntoConstraints = false
+        buttonStart.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        buttonStart.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 40).isActive = true
+        
+        buttonEnd.backgroundColor = .red
+        buttonEnd.layer.cornerRadius = 15
+        buttonEnd.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        buttonEnd.translatesAutoresizingMaskIntoConstraints = false
+        buttonEnd.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        buttonEnd.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -40).isActive = true
+
+        
+        
+        
+        
+
         // Do any additional setup after loading the view.
 
         
